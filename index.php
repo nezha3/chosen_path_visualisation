be2f009647009d037054f1866b348010e9ce2b65
Oliver Chi <oliver.chi.ict@icloud.com>
2018-05-21 20:43:03 +1000

    init

---
+++ index.php
@@ -0,0 +1,39 @@
+<?
+//php file header
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+  $file = '/tmp/sample-app.log';
+  $message = file_get_contents('php://input');
+  file_put_contents($file, date('Y-m-d H:i:s') . " Received message: " . $message . "\n", FILE_APPEND);
+}
+else
+{
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+
+    <title>Chosen Path Visualisation</title>
+
+    <script src="//d3js.org/d3.v3.min.js"></script><!-- D3 JS -->
+    <link rel="stylesheet" type="text/css"
+      href="https://fonts.googleapis.com/css?family=Open+Sans:400,600"><!-- google font -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+    </head>
+
+  <body>
+
+
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="chart.js"></script>
+  </body>
+</html>
+
+<?
+}
+?>
